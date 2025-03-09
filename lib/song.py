32db2aebaa604016befb9be87ce55867db5fbfa4
MarkKibet <kemboimarkbiwott@gmail.com>
2025-03-09 18:24:16 +0300

    Worked on the song model

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.genre = genre
+        self.artist = artist
+        Song.count += 1
+
+        #track genres and artists
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+
+        #Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        #Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
